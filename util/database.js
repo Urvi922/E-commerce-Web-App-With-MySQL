b207b68d569767d68d77774c87998e25c2cade9e
Urvi Patel <urvipatel922@gmail.com>
2024-08-23 18:08:22 -0400

    Update database.js
    for deployment

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('nodejs', 'root', 'MYSQLl0g!n', {
-    dialect: 'mysql',
-    host: 'localhost'
+// const sequelize = new Sequelize('nodejs', 'root', 'MYSQLl0g!n', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+
+const sequelize = new Sequelize('ROOT_NAME', 'NAME', 'PASSWORD', {
+    dialect: 'DIALECT',
+    host: 'HOST'
 });
 
 module.exports = sequelize;
